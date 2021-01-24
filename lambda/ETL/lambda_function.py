3ebe5be6b45dfbe6fd315892c8e615a3b20fd8ae
Jannic Cutura <Janniccutura@gmail.com>
2021-01-24 08:37:22 +0100

    Added lambda outputting to s3

--- lambda/ETL/lambda_function.py
+++ lambda/ETL/lambda_function.py
@@ -8,17 +8,18 @@ from urllib.parse import unquote_plus
 s3_client = boto3.client('s3')
 s3 = boto3.resource('s3')
 
-from xml_2_data import mnfp_2_data, mnfp1_2_data, nmfp2_2_data
+from xml_2_data import mnfp_2_data
+from xml_2_data import mnfp1_2_data
+from xml_2_data import mnfp2_2_data
 
 
 def lambda_handler(event, context):
     # parse the S3 triggered event
-    # record =event['Records'][0]
-    # bucket = record['s3']['bucket']['name']
-    # key = unquote_plus(record['s3']['object']['key'])
-
-    bucket = "fundmapper"
-    key = "02-RawNMFPs/S000008702/2011-03-07-S000008702.txt"
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = unquote_plus(record['s3']['object']['key'])
+    # bucket = "fundmapper"
+    # key = "02-RawNMFPs/S000008702/2011-03-07-S000008702.txt"
 
     prefix, series_id, filing = key.split("/")
 
@@ -29,7 +30,7 @@ def lambda_handler(event, context):
     filing = open("/tmp/" + series_id + "_" + filing + ".txt", 'r').read()
     filing = filing.replace(":", "")
     filing_type = re.search("<TYPE>(.*)\n", filing).group(1)
-    filing_date = re.sub("[^0-9]", "", re.search("CONFORMED PERIOD OF REPORT(.*)\n", filing).group(1))[0:6]
+    filing_date = int(re.sub("[^0-9]", "", re.search("CONFORMED PERIOD OF REPORT(.*)\n", filing).group(1))[0:6])
     filing = (filing.replace("\n", "")
               .replace(' xmlns="http//www.sec.gov/edgar/nmfpsecurities"', '')
               .replace(' xmlns="http//www.sec.gov/edgar/nmfpfund"', ""))
@@ -43,8 +44,29 @@ def lambda_handler(event, context):
     if filing_type in ["N-MFP2", "N-MFP2/A"]:
         series_df, class_df, holdings, all_collateral = mnfp2_2_data(filing)
 
-    series_df.to_csv("/tmp/" + series_id + "_" + filing + ".csv")
-    # save new version to S3
-    s3_client.upload_file('/tmp/testfile.txt', "fundmapper", "thisismynewtest/placedhere.txt")
+    series_df['date'] = filing_date
+    class_df['date'] = filing_date
+    holdings['date'] = filing_date
+    all_collateral['date'] = filing_date
 
-    return series_df.shape[1]
+    series_df.to_csv("/tmp/series_" + series_id + "_" + str(filing_date) + ".csv")
+    s3_client.upload_file("/tmp/series_" + series_id + "_" + str(filing_date) + ".csv", "fundmapper",
+                          "03-ParsedRecords/series_data/" + series_id + "/" + series_id + "_" + str(
+                              filing_date) + ".csv")
+
+    class_df.to_csv("/tmp/class_" + series_id + "_" + str(filing_date) + ".csv")
+    s3_client.upload_file("/tmp/class_" + series_id + "_" + str(filing_date) + ".csv", "fundmapper",
+                          "03-ParsedRecords/class_data/" + series_id + "/" + series_id + "_" + str(
+                              filing_date) + ".csv")
+
+    holdings.to_csv("/tmp/holdings_" + series_id + "_" + str(filing_date) + ".csv")
+    s3_client.upload_file("/tmp/holdings_" + series_id + "_" + str(filing_date) + ".csv", "fundmapper",
+                          "03-ParsedRecords/holdings_data/" + series_id + "/" + series_id + "_" + str(
+                              filing_date) + ".csv")
+
+    all_collateral.to_csv("/tmp/collateral_" + series_id + "_" + str(filing_date) + ".csv")
+    s3_client.upload_file("/tmp/collateral_" + series_id + "_" + str(filing_date) + ".csv", "fundmapper",
+                          "03-ParsedRecords/collateral_data/" + series_id + "/" + series_id + "_" + str(
+                              filing_date) + ".csv")
+
+    return "Success"
--- webscrap-sec.R
+++ webscrap-sec.R
@@ -1,67 +1,9 @@
-rm(list=ls()) # clear env
-cat('\014')
-libaries =c('dplyr','tidyverse',"edgarWebR","aws.s3","aws.ec2metadata","aws.dynamodb","RStudioAMI","stringr")  # define libraries
-lapply(libaries, require, character.only=TRUE)
-svc <- paws::dynamodb(config = list(region = "eu-central-1"))
-
-# wrapper to read csv from S3
-s3.read_csv <- function(s3_path, sep=",", row.names= NULL) {
-  s3_pattern <- "^s3://(.+?)/(.*)$"
-  s3_bucket <- gsub(s3_pattern, "\\1", s3_path)
-  s3_object <- gsub(s3_pattern, "\\2", s3_path)
-  read.csv(text = rawToChar(aws.s3::get_object(s3_object, s3_bucket)),  row.names=row.names, sep = sep)
-}
-
-
-series_ids= s3.read_csv("s3://fundmapper/series_ids.csv",  row.names=NULL, sep = ",") 
-series_ids = as.character(series_ids$series_ids)
-
-
-
-
-
-
-
-## get last 5 months
-for (series_id in series_ids) {
-  year = as.numeric(substr(Sys.time(),1,4))
-  print(paste0("Parsing year ", year, sep=""))
-  
-  # access filings and keep the newest filing per month (i.e. the amended form if multiples exist) and keep the last 5 records
-  filings = company_filings(series_id,type ="N-MFP", count=100, before=paste0(year+1,"0101", sep="")) %>%
-    mutate(filing_date = as.character(filing_date),
-           filing_year = substr(filing_date,1,4),
-           filing_month= substr(filing_date,6,7)) %>%
-    group_by(filing_year, filing_month) %>%
-    top_n(1,filing_date) %>% head(5) %>%
-    mutate(url = str_replace(href,"-index.htm",".txt"))
-  
-  
-  for (row in 1:nrow(filings))   {
-    # prepare file identifier
-    url = filings$url[row]
-    filing_date = filings$filing_date[row]
-    
-    if (!object_exists(bucket = "fundmapper", object =  paste0("02-RawNMFPs/",series_id,"/",filing_date,"-",series_id,".txt"))) {
-      # download file and save locally 
-      download.file(url, destfile = "/home/rstudio/tmp.txt")
-      
-      # put to S3 data lake
-      put_object(file = "tmp.txt", object = paste0("02-RawNMFPs/",series_id,"/",filing_date,"-",series_id,".txt"), bucket = "fundmapper")
-      file.remove("/home/rstudio/tmp.txt")
-    }
-  }
-  rm(filings)
-  
-}
-
-
 
 
 ## get legacy documents
 start_time <- Sys.time()
 counter = 1
-for (series_id in series_ids[1]) {
+for (series_id in series_ids[1:2]) {
   print(paste0("This is for fund (", series_id,"), ", counter, " of ", length(series_ids),  sep=""))
   counter = counter + 1 
   for (year in seq(2011,2020,1)) {
@@ -111,27 +53,6 @@ for (series_id in series_ids[1]) {
 
 end_time <- Sys.time()
 end_time - start_time
-
-
-
-
-
-
-#create a list of series_ids
-mmflists = get_bucket(bucket = "fundmapper", "01-MMFLists/")
-mmflists = map_df(mmflists, ~ map_df(.x, ~ replace(.x, is.null(.x), NA)), .id = "my.var") %>%
-  select(Key) %>%
-  filter(str_detect(Key, ".csv"))
-series_ids= list()
-for (row in 1:nrow(mmflists))   {
-  key = as.character(mmflists[row,"Key"])
-  print(key)
-  tmp = as.character(unique(s3.read_csv(paste0("s3://fundmapper/", key, sep=""), sep=";")$series_id))
-  series_ids = union(series_ids, tmp)
-}
-series_ids = as.character(series_ids)
-series_ids = data.frame(series_ids)
-s3write_using(series_ids, bucket = "fundmapper", object = "series_ids.csv", FUN = utils::write.csv)
 
 
 
